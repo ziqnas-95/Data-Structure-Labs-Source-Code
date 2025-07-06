44cded5cdc321baa17e13a1f40328ba8b7e2639d
Ziqnas <haziqnasaruddin@gmail.com>
2025-07-06 23:25:26 +0800

    FileIO : CSV

---
+++ src/FileIO/ReadCSV.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package FileIO;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.*;
+import java.util.Scanner;
+/**
+ *
+ * @author Haziq Nasaruddin
+ */
+
+
+
+public class ReadCSV {
+    
+    public static void main(String[] args) {
+        
+        try (BufferedReader reader = new BufferedReader(new FileReader("sample_students.csv"))){
+            
+            String line;
+            int count = 0;
+            while ((line = reader.readLine()) != null){
+                String[] values = line.split(",");
+                count++;
+                if (count != 1){
+                    for (String x : values){
+                        System.out.print(x.trim() + "\t\t");
+                    }
+                    System.out.println("");
+                }
+                
+            }
+
+        } catch (IOException e){
+            System.out.println("Error reading CSV file: " + e.getMessage());
+        }
+        
+    }
+}
