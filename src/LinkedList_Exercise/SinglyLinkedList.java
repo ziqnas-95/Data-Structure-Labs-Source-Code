bed688aecd1c6f1937a584d8a959018694fd42d0
Ziqnas <haziqnasaruddin@gmail.com>
2025-07-07 00:59:11 +0800

    SinglyLinkedList Exercise

---
+++ src/LinkedList_Exercise/SinglyLinkedList.java
@@ -0,0 +1,170 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LinkedList_Exercise;
+
+/**
+ *
+ * @author Haziq Nasaruddin
+ */
+
+class Node<T> {
+
+    T element;
+    Node<T> next;
+
+    public Node(T data){
+        this.element = data;
+        this.next = null;
+    }
+}
+
+public class SinglyLinkedList<T> {
+    
+    private Node head, tail;
+    int size;
+    
+    public SinglyLinkedList(){
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+    
+    public void addFirst(T e){
+        
+        Node<T> newNode = new Node<T>(e);
+        newNode.next = head;
+        head = newNode;
+        size++;
+        
+        if (tail == null){
+            tail = head;
+        }
+    }
+    
+    public void addLast(T e){
+        
+        Node<T> newNode = new Node<T>(e);
+        
+        if (tail == null){
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = tail.next;
+        }
+        size++;
+    }
+    
+    public void add(int index, T e){
+    
+        if (index == 0){
+            addFirst(e);
+        } else if (index >= size) {
+            addLast(e);
+        } else {
+            Node<T> current = head;
+            for (int i = 1; i < index; i++){
+                current = current.next;
+            }
+            Node<T> temp = current.next;
+            current.next = new Node<T>(e);
+            current.next.next = temp;
+            size++;
+        }
+    }
+    
+    
+    public T removeFirst(){
+    
+        if (size == 0)
+            return null;
+        else {
+            Node<T> temp = head;
+            head = head.next;
+            size--;
+            
+            if (head == null){
+                tail = null;
+            }
+            return temp.element;
+        }
+    }
+    
+    public T removeLast(){
+        
+        if (size == 0){
+            return null;
+        }
+        else if (size == 1){
+            Node<T> temp = head;
+            head = tail = null;
+            size = 0;
+            return temp.element;
+        } else {
+            Node<T> current = head;
+            
+            for (int i = 0; i < size - 2; i++){
+                current = current.next;
+            }
+            
+            Node<T> temp = tail;
+            tail = current;
+            tail.next = null;
+            size--;
+            return temp.element;
+        }
+    }
+    
+    public T remove(int index){
+        if (index < 0 || index >= size){
+            return null;
+        }
+        else if (index == 0) {
+            return removeFirst();
+        }
+        else if (index == size - 1){
+            return removeLast();
+        }
+        else {
+            Node<T> previous = head;
+            for (int i = 1; i < index; i++) {
+                previous = previous.next;
+            } 
+            
+            Node<T> current = previous.next;
+            previous.next = current.next;
+            size--;
+            return current.element;
+        }
+    
+    
+    }
+    
+    public void traverse(){
+        
+        Node<T> current = head;
+        
+        while(current.next != null){
+            System.out.print(current.element + " ");
+            current = current.next;
+        }
+        System.out.print(current.element);
+    
+    }
+    
+    public static void main(String[] args) {
+        
+        SinglyLinkedList<String> hehe = new SinglyLinkedList<String>();
+        
+        hehe.addFirst("Haziq");
+        hehe.addLast("bestie");
+        hehe.add(1, "adam");
+        hehe.removeFirst();
+        hehe.removeLast();
+        
+        hehe.traverse();
+        
+    }
+    
+}
